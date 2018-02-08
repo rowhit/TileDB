6bf1b4e868be95ddc64886c9f5ebba891620f074
Tyler Denniston <tyler@csail.mit.edu>
2018-02-08 17:40:02 -0500

    Change install-deps.sh to call other install scripts as necessary.

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,6 +4,59 @@ die() {
   echo "$@" 1>&2 ; popd 2>/dev/null; exit 1
 }
 
+arg() {
+    echo "$1" | sed "s/^${2-[^=]*=}//" | sed "s/:/;/g"
+}
+
+usage() {
+echo '
+Usage: '"$0"' [<options>]
+    Installs required and optional dependencies for TileDB.
+
+Options: [defaults in brackets after descriptions]
+Configuration:
+    --help                          print this message
+    --enable-hdfs                   installs the hdfs storage backend requirements
+    --enable-s3                     installs the s3 storage backend requirements
+    --enable=arg1,arg2...           same as "--enable-arg1 --enable-arg2 ..."
+
+Dependencies:
+    c/c++ compiler
+    GNU make
+    cmake           http://www.cmake.org/
+
+Example:
+    $0 --enable=hdfs,s3
+'
+    exit 10
+}
+
+# Parse arguments
+enable_s3=0
+enable_hdfs=0
+enable_multiple=""
+while test $# != 0; do
+    case "$1" in
+    --enable-hdfs) enable_hdfs=1;;
+    --enable-s3) enable_s3=1;;
+    --enable=*) s=`arg "$1"`
+                enable_multiple="$s";;
+    --help) usage ;;
+    *) die "Unknown option: $1" ;;
+    esac
+    shift
+done
+
+# Parse the comma-separated list of enables.
+IFS=',' read -ra enables <<< "$enable_multiple"
+for en in "${enables[@]}"; do
+  case "$en" in
+    s3) enable_s3=1;;
+    hdfs) enable_hdfs=1;;
+    *) die "Unknown option: --enable-$en" ;;
+  esac
+done
+
 build_install_cmake() {
 wget -P /tmp https://cmake.org/files/v3.9/cmake-3.9.4-Linux-x86_64.tar.gz \
     && tar xzf /tmp/cmake-3.9.4-Linux-x86_64.tar.gz -C /tmp \
@@ -49,7 +102,7 @@ install_brew_pkgs() {
   brew install tiledb-inc/stable/blosc || die "could not install blosc pkg dependency"
 }
 
-install_deps() {
+install_base_deps() {
   if [[ $OSTYPE == linux* ]]; then
     if [ -n "$(command -v apt-get)" ]; then
       install_apt_pkgs 
@@ -78,8 +131,20 @@ install_deps() {
   fi 
 }
 
+install_extra_deps() {
+  if [[ $enable_s3 -eq 1 ]]; then
+      echo Installing S3 dependencies...
+      source install-s3.sh && source install-minio.sh
+  fi
+  if [[ $enable_hdfs -eq 1 ]]; then
+      echo Installing HDFS dependencies...
+      source install-hadoop.sh
+  fi
+}
+
 run() {
-  install_deps
+  install_base_deps
+  install_extra_deps
 }
 
 run 
